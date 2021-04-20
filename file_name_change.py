a3fd53504a2f9712f0c593b1db026f51760dbfd6
ksh <hyun031916@gmail.com>
2021-04-21 04:51:24 +0900

    mask 착용 유무 모델 정확도를 높이기 위한 자료 수집

--- file_name_change.py
+++ file_name_change.py
@@ -1,15 +1,20 @@
 import cv2
-import cvlib as cv
 import os
 
 
 
-img_dir = ['mask', 'nomask']
+# img_dir = ['mask', 'nomask']
+#
+# for dir in img_dir:
+#     file = 1
+#     for file_name in os.listdir(dir):
+#         img = cv2.imread(dir+'/'+file_name)
+#         cv2.imwrite('img/'+dir + '/' + dir + str(file) + '.jpg', img)
+#         file += 1
+# cv2.destroyAllWindows()
 
-for dir in img_dir:
-    file = 1
-    for file_name in os.listdir(dir):
-        img = cv2.imread(dir+'/'+file_name)
-        cv2.imwrite('img/'+dir + '/' + dir + str(file) + '.jpg', img)
-        file += 1
+file = 1
+for file_name in os.listdir('img/nomask'):
+    os.rename('img/nomask/'+file_name,'img/nomask/nomask'+str(file))
+    file += 1
 cv2.destroyAllWindows()
